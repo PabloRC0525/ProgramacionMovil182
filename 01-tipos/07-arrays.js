35b26c66765a89c82c26eab5a1278e2fd049fe7c
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 12:48:41 -0600

    Array

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,11 @@
+let animales = ['chanchito', 'caballo'];
+console.log(animales);
+console.log(animales[0]);
+animales[2] = 'dragón';
+console.log(animales);
+
+animales[10] = 'Pez';
+console.log(animales[7]);
+console.log(typeof animales);
+animales.length
+console.log(animales.length);
