43c3af5678a5d7c0a436f3f9f05890f292f2fa0b
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 15:28:44 -0600

    Intro

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,12 @@
+let user = {
+    email: 'nico@holamundo.io',
+    name: 'Nicolas',
+    direccion: {
+        calle: 'Queen st',
+        numero: 15,
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log('Recuperando clave...');
+    },
+};
