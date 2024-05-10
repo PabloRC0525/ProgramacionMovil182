b420444881ab02236dd8ff3cf970e07ab83522b8
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 15:33:51 -0600

    dinamico

---
+++ 05-objetos/02-dinamico.js
@@ -0,0 +1,16 @@
+const user = { id: 1 };
+
+user.name = 'Nicolas';
+user.guardar = function () {
+    console.log('Guardando', user.name);
+}
+user.guardar()
+
+delete user.name;
+delete user.guardar;
+console.log(user);
+
+const user1 = Object.seal({ id:1 });
+user1.name = 'Nico';
+user1.id = 2;
+console.log(user1);
