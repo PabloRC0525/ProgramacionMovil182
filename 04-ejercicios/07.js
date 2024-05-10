97caec2643c5dacff05b9633cecf522b40cad45b
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 15:07:10 -0600

    Ejercicio 7

---
+++ 04-ejercicios/07.js
@@ -0,0 +1,12 @@
+/**
+ * Crear algoritmo que devuelva
+ * el precio del producto
+ * más el impuesto
+ */
+
+function precioCompleto(precio, impuesto) {
+    return precio + precio * impuesto;
+}
+
+let resultado = precioCompleto(19.90, 0.15);
+console.log(resultado);
