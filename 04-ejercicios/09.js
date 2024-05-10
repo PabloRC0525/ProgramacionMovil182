7067cee1389513541025f73f2620e42d363c0e63
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 15:19:18 -0600

    Ejercicio 9

---
+++ 04-ejercicios/09.js
@@ -0,0 +1,31 @@
+/**
+ * crear algoritmo que tome un array de
+ * objetos y devuelva un array de pares
+ */
+let pairs = [
+    [1, {name: "Nicolas"}],
+    [2, {name: "Felipe"}],
+    [3, {name: "Chanchito"}]
+]
+
+let array = [{
+    id: 1,
+    name: 'Nicolas',
+}, {id: 2,
+    name: 'Felipe',
+}, {id: 3,
+    name: 'Chanchito',
+}]
+
+function toCollection(arr) {
+    let collection = [];    
+    for (idx in arr) {
+        let elemento = arr[idx];
+        collection[idx] = elemento [1];
+        collection[idx].id = elemento [0];
+    }
+    return collection;
+}
+
+let resultado = toCollection(pairs);
+console.log(resultado);
