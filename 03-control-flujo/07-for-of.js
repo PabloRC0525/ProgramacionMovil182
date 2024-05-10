a319c6c70536a34174e4d87dc90b997f738cb2d1
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 14:19:33 -0600

    For of

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,15 @@
+// while
+// do while
+// for
+
+let animales = ['Chanchito feliz', 'Dragón', 'Perrito'];
+
+for (let animal of animales){
+    console.log(animal);
+}
+
+let i = 0;
+while (i < animales.length) {
+    console.log(animales[i]);
+    i++;
+}
