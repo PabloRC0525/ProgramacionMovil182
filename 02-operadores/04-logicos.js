c6950df0edb03ae4709eb688ae56222fc28dd677
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 13:30:02 -0600

    Operador lógico

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,15 @@
+// AND, OR, NOT
+
+// And &&
+let mayor = true;
+let suscrito = true;
+//console.log(true && true);
+//console.log(true && false);
+console.log('operador AND', mayor && suscrito);
+
+// OR ||
+console.log('operador OR', mayor || suscrito);
+
+// NOT !
+console.log('operador NOT',!mayor);
+let soloCatalogoInfantil = !mayor;
