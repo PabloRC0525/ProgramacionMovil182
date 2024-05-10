72a2466232797b033e5513e2b9130edc409c383a
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 13:56:38 -0600

    Else

---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,10 @@
+let edad = 28;
+if (edad > 17) {
+    console.log('Usuario mayor de edad')
+}
+else if(edad > 13) {
+    console.log('Usuario necesita estar acompañado de sus padres')
+}
+else{
+    console.log('Usuario menor de edad')
+}
