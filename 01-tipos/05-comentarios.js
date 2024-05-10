0ebd6ae1a7548c162f12919bd4ab3047c9d3de45
PabloRC0525 <121038827@upq.edu.mx>
2024-05-10 12:29:11 -0600

    Comentarios

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,15 @@
+// notehu  nthoeunhtoeu
+/*
+Tolo lo que escribamos entremedio
+de los slash y los asteriscos
+va a ser ignorado
+*/
+/**
+ * Este es un comentario
+ * de
+ * muchas
+ * líneas
+ */
+let numero = 42; // Este es otro comentario
+
+console.log(numero);
