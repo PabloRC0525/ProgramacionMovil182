687d983ae07f114c147a37a799a2e5e355545639
PabloRC0525 <121038827@upq.edu.mx>
2024-05-20 15:41:33 -0600

    Avance Pract2

---
+++ JS4React/index.js
@@ -0,0 +1,32 @@
+
+//function
+
+/*function suma(a,b){
+    return a+b;
+}
+
+console.log(suma(2,3));
+
+//function fatArrow
+const suma=(a,b)=>{
+    return a+b;
+}
+
+const suma=(a,b)=> a+b;
+
+
+console.log(suma(2,3));
+*/
+const usuario={
+    nombre:'Ivan',
+    apellido:'Guerra',
+    edad: 36,
+    direccion: {
+        pais: 'Mexico',
+        ciudad: 'Queretaro',
+        calle: 'La misma 123'
+    },
+    amigos: ['Alex', 'Carlos'],
+    estatus: true,
+    enciaCorreo: enviarCorr=()=>'enviando...'
+}
