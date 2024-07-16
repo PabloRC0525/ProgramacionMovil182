3b6a31fb6850492681c9a5f56fd0045eabc0d7c8
PabloRC0525 <121038827@upq.edu.mx>
2024-07-15 22:48:37 -0600

    Se finalizó la practica 13

--- Pract9/screens/Prueba.js
+++ Pract9/screens/Prueba.js
@@ -1,50 +1,49 @@
 import { Entypo, FontAwesome5 } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import { View, Text, Modal, Dimensions, TouchableOpacity } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import BottomSheet, {BottomSheetModal, BottomSheetModalProvider, } from "@gorhom/bottom-sheet";
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const {width, height} = Dimensions.get("window");
 export default function Prueba() {
+    
     const [modal, setModal] = useState(false);
-    return(
-        <View 
-            style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-        }}>
-            {/* Estructura basica del modal */}
-            <Modal visible={modal} animationType="fade">
-                    <View style={{
-                        flex:1,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        backgroundColor:"rgba(0,0,0,1)",
-                    }}>
-                        <TouchableOpacity onPress={()=> setModal(false)}>
-                        <FontAwesome5 name='zhihu' size={width * 0.1} color="#fff"/>
-                        </TouchableOpacity>
-                    </View>
-            </Modal>
-            <TouchableOpacity
-            onPress={() => setModal(true)}
-            style={{
-                width: width * 0.6,
-                height: height * 0.1,
-                backgroundColor: "#000",
-                borderRadius: width * 0.02,
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
-                <Text style={{
-                    fontSize: width * 0.04,
-                    fontWeight: "700",
-                    color:"#fff",
-                }}>
-                    Abrir Modal
-                </Text>
+    const bottomSheetModalRef = useRef(null);
+    const snapPoints = [width * 0.3, width * 0.5, width * 0.9];
+    
+    const hey = () => {
+        bottomSheetModalRef?.current?.present()
 
-            </TouchableOpacity>
+    }
+
+    return(
+        <GestureHandlerRootView style={{flex:1}}>
+        <BottomSheetModalProvider>
+        <BottomSheetModal name='pablo'
+        ref={bottomSheetModalRef}
+        snapPoints={[height*0.4, height*0.6, height*0.9 ]}
+        >
+            <Text>gei</Text> 
+        </BottomSheetModal>
+        <View style={{
+            flex:1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#000'
+        }}>
+        <TouchableOpacity onPress={()=>hey()} style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: width * 0.1,
+            height: height * 0.1,
+            backgroundColor: '#ffff'
+        }}>
+                <Text>Xd</Text>
+        </TouchableOpacity>
+
 
         </View>
-    )
+        </BottomSheetModalProvider>
+        </GestureHandlerRootView>
+    );
 }
